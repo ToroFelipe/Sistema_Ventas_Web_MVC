04265125a91bb0ac59237e971e3ba015fa4eefd8
pipe <felipe.toro.d@hotmail.com>
2021-03-28 22:43:01 -0300

    primer commit del proyecto

---
+++ src/java/Config/Conexion.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Conexion {
+    Connection con;
+    String url ="jdbc:mysql://localhost:3306/bd_ventas";
+    String user="root";
+    String pass="rootroot";
+    public Connection Conexion(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection(url,user,pass);
+        } catch (Exception e) {
+        }
+            
+            
+    return con;
+    }
+}
